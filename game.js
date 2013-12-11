25ea1656591b01af830ead8d5f0481d66946430f
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-12-11 16:20:15 -0500

    Trying ...

--- game.js
+++ game.js
@@ -1,181 +1,202 @@
 var key = {
-    up:    38,
-    down:  40,
-    left:  37,
+    up: 38,
+    down: 40,
+    left: 37,
     right: 39
 }
 
-function SnakeTail(x, y) {
-    //store each position frame
-    this.x = x;
-    this.y = y;
-    this.vx = 0;
-    this.vy = 0;
-    this.nextTail = null;
-    this.queue = [];
-    this.queue.push({vx: this.vx, vy: this.vy, expiresIn: null});
-    SnakeTail.prototype.update = function() {
+var SCALE = 20;
 
-        this.x += this.queue[0].vx;
-        this.y += this.queue[0].vy;
+function Food(x, y) {
+    this.position = {
+        x: x,
+        y: y
+    };
 
-        if (this.queue[0].expiresIn === null) {
+    Food.prototype.update = function() {
 
-        } else {
-            if (this.queue[0].expiresIn !== null){
-                this.queue[0].expiresIn--;
-                if (this.queue[0].expiresIn <= 0) {
-                    console.log(this.queue[0].expiresIn);
-                    this.queue.shift();
-                } else {
+    };
 
-                }
+    Food.prototype.render = function(graphics) {
+        graphics.drawRect("red", this.position.x , this.position.y, 10, 10);
+    };
+
+    Food.prototype.collideWith = function(snake) {
+
+        var position = snake.parts[snake.parts.length - 1];
+        var headX = (position.x);
+        var headY = (position.y) ;
+
+        var foodX = (this.position.x);
+        var foodY = (this.position.y );
+
+        if (headX >= foodX - 5 && headX <= foodX + 5) {
+            if (headY >= foodY - 5 && headY <= foodY + 5) {
+                console.log('colliding')
+                return true;
             }
         }
 
+        return false;
     };
 
-    SnakeTail.prototype.render = function(ctx) {
-        ctx.setFillColor("red")
-        ctx.fillRect(this.x, this.y, 20, 20);
-    };
-
-    SnakeTail.prototype.setDirection = function(vx, vy, changesIn) {
-        if (this.nextTail != null) {
-            this.nextTail.setDirection(this.vx, this.vy, changesIn);
-        }
-console.log(changesIn);
-
-        this.queue[this.queue.length -1].expiresIn = changesIn;
-
-        if (this.vx == 0 && this.vy == 0)
-            this.queue[0].expiresIn = 20;
-
-        this.queue.push({vx: vx , vy: vy, expiresIn: null});
-
+    Food.prototype.reset = function() {
+        this.position.x = Math.floor(Math.random() * 400);
+        this.position.y = Math.floor(Math.random() * 400);
 
 
     };
 }
 
-function Snake(x, y, keys) {
+function Snake(x, y, keys, game) {
     //Snake position
     this.x = x;
     this.y = y;
     this.vx = 0;
     this.vy = 0;
     this.keys = keys;
-    this.tail = null;
-    this.step = 0;
-    this.lastKey = null;
-    this.updatesSinceLastDirectionChange = 0;
+    this.parts = [];
+    this.speed = 1;
+    this.game = game;
+    this.size = 10;
+
+    this.parts.push({
+        x: x,
+        y: y
+    });
 
     Snake.prototype.update = function() {
 
-
-
-
         var keyPressed = null;
         if (this.keys[key.up]) {
-            this.vy = -1;
+            this.vy = -this.speed;
             this.vx = 0;
 
             keyPressed = key.up;
 
         } else if (this.keys[key.down]) {
-            this.vy = 1;
+            this.vy = this.speed;
             this.vx = 0;
 
             keyPressed = key.down;
-        }else if (this.keys[key.right]) {
-            this.vx = 1;
+        } else if (this.keys[key.right]) {
+            this.vx = this.speed;
             this.vy = 0;
 
             keyPressed = key.right;
-        }else if (this.keys[key.left]) {
-            this.vx = -1;
+        } else if (this.keys[key.left]) {
+            this.vx = -this.speed;
             this.vy = 0;
 
             keyPressed = key.left;
         }
 
-        if (this.vx > 0 || this.vy > 0)
-            this.updatesSinceLastDirectionChange++;
+        for(var i = 0; i < this.parts.length; i++) {
+            
+            var current = this.parts[i];
+            if (i == this.parts.length - 1) {
+                current.x += this.vx,
+                current.y += this.vy
+            } else {
+                var nextInLine = this.parts[i + 1];    
 
-
-        if (keyPressed !== null && keyPressed !== this.lastKey) {
-            this.lastKey = keyPressed;
-            if (this.tail != null) {
-                this.tail.setDirection(this.vx, this.vy, this.updatesSinceLastDirectionChange)
-                this.updatesSinceLastDirectionChange = 0;
+                current.x = nextInLine.x;
+                current.y = nextInLine.y;    
             }
         }
+        
 
-
-
-
-
-        this.x += this.vx;
-        this.y += this.vy
-
-
-
-
-
-        if (this.tail != null) {
-            this.tail.update();
+        if (game.food.collideWith(this)) {
+            this.addPart();
+            game.food.reset();
         }
-
-
+     
     },
 
-    Snake.prototype.render = function(ctx) {
-        ctx.setFillColor("black")
-        ctx.fillRect(this.x, this.y, 20, 20);
-        if (this.tail != null) {
-            this.tail.render(ctx);
-        }
+    Snake.prototype.render = function(graphics) {
+        graphics.drawLine("black", this.parts, 10);
     }
+
+    Snake.prototype.addPart = function() {
+        var last = this.parts[0];
+        for (var i = 0; i < this.size; i++) {
+            this.parts.splice(0, 0, {
+                x: last.x,
+                y: last.y
+            });    
+        }
+
+        document.getElementById("size").innerHTML = ((this.parts.length - 1) / 10) + 1;
+        
+    }
+}
+
+function Graphics(ctx) {
+    this.ctx = ctx;
+
+    Graphics.prototype.drawLine = function(color, path, width) {
+        console.log(path[0])
+        this.ctx.setFillColor(color);
+        this.ctx.lineWidth= width;
+        this.ctx.beginPath();
+        this.ctx.moveTo(path[0].x, path[0].y );
+        for(var i = 0; i < path.length; i++) {
+            var point = path[i];
+            this.ctx.lineTo(point.x, point.y);    
+        }
+        this.ctx.stroke();
+    }
+
+    Graphics.prototype.drawRect = function(color, x, y, width, height) {
+        this.ctx.setFillColor(color)
+        this.ctx.fillRect(x - (width / 2), y - (height / 2), width, height);
+    };
+
 }
 
 function Game() {
     var game = this;
     this.keys = [];
-
-    window.addEventListener("keydown", function (e) {
-        game.keys[e.keyCode] = true;
-    });
-
-    window.addEventListener("keyup", function (e) {
-        game.keys[e.keyCode] = false;
-    });
-
     this.canvas = document.getElementById("game");
     this.ctx = this.canvas.getContext("2d");
+    this.graphics = new Graphics(this.ctx);
 
-    this.snake = new Snake(50, 50, this.keys);
-    this.snake.tail = new SnakeTail(50, 50)
+    window.addEventListener("keydown", function(e) {
+        game.keys[e.keyCode] = true;
+    }, true);
 
+    window.addEventListener("keyup", function(e) {
+        game.keys[e.keyCode] = false;
+    }, true);
+
+
+    this.food = new Food(30, 30);
+    this.snake = new Snake(1, 1, this.keys, game);
 
     Game.prototype.start = function() {
         (function loop() {
             requestAnimationFrame(loop);
             game.render();
+            game.update();
         })();
+
+       
+    };
+
+    Game.prototype.update = function() {
+        game.snake.update();
     };
 
     Game.prototype.render = function() {
-        game.snake.update();
+     
         this.ctx.save();
         this.ctx.setTransform(1, 0, 0, 1, 0, 0);
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.restore();
-        game.snake.render(game.ctx);
+        game.snake.render(this.graphics );
+        game.food.render(this.graphics);
     }
 
 }
 
 new Game().start();
-
-
-
