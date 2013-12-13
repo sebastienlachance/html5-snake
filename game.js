ba144965bf5414656d2043149d897540e7e25128
Sebastien Lachance <sebastien.lachance@creatiom.com>
2013-12-13 14:44:29 -0500

    Done, but needs more work

--- game.js
+++ game.js
@@ -5,48 +5,43 @@ var key = {
     right: 39
 }
 
-var SCALE = 20;
+var SCALE = 10;
 
-function Food(x, y) {
-    this.position = {
-        x: x,
-        y: y
-    };
+function Food() {
+
+    this.position = {};
+    this.reset();
 
     Food.prototype.update = function() {
 
     };
 
     Food.prototype.render = function(graphics) {
-        graphics.drawRect("red", this.position.x , this.position.y, 10, 10);
+        graphics.drawRect("red", this.position.x  * SCALE , this.position.y * SCALE, SCALE, SCALE);
     };
 
     Food.prototype.collideWith = function(snake) {
-
-        var position = snake.parts[0];
-        var headX = (position.x);
-        var headY = (position.y) ;
+        var head = snake.parts[0];
+        var headX = head.x;
+        var headY = head.y;
 
         var foodX = (this.position.x);
         var foodY = (this.position.y );
 
-        if (headX >= foodX - 5 && headX <= foodX + 5) {
-            if (headY >= foodY - 5 && headY <= foodY + 5) {
-                console.log('colliding')
-                return true;
-            }
+        if (headX == foodX && headY == foodY) { 
+            return true;
         }
 
         return false;
     };
 
-    Food.prototype.reset = function() {
-        this.position.x = Math.floor(Math.random() * 400);
-        this.position.y = Math.floor(Math.random() * 400);
-
-
-    };
+   
 }
+Food.prototype.reset = function() {
+    this.position.x = Math.round(Math.random() * (game.width - SCALE) / SCALE);
+    this.position.y = Math.round(Math.random() * (game.height - SCALE) / SCALE);
+};
+
 
 function Snake(x, y, keys, game) {
     //Snake position
@@ -56,9 +51,9 @@ function Snake(x, y, keys, game) {
     this.vy = 0;
     this.keys = keys;
     this.parts = [];
-    this.speed = 0.1;
+    this.speed = 1;
     this.game = game;
-    this.size = 10;
+    this.lastDirection = null;
 
     this.parts.push({
         x: x,
@@ -67,80 +62,81 @@ function Snake(x, y, keys, game) {
 
     Snake.prototype.update = function() {
 
-        var keyPressed = null;
+        
         if (this.keys[key.up]) {
-            this.vy = -this.speed;
-            this.vx = 0;
-
-            keyPressed = key.up;
+            if (this.lastDirection != key.down) {
+                this.vy = -this.speed;
+                this.vx = 0;
+                this.lastDirection = key.up;    
+            }
 
         } else if (this.keys[key.down]) {
-            this.vy = this.speed;
-            this.vx = 0;
-
-            keyPressed = key.down;
+            if (this.lastDirection != key.up) {
+                this.vy = this.speed;
+                this.vx = 0;
+                this.lastDirection = key.down;    
+            }
+            
         } else if (this.keys[key.right]) {
-            this.vx = this.speed;
-            this.vy = 0;
-
-            keyPressed = key.right;
+            if (this.lastDirection != key.left) {
+                this.vx = this.speed;
+                this.vy = 0;
+                this.lastDirection = key.right;     
+            }
+        
         } else if (this.keys[key.left]) {
-            this.vx = -this.speed;
-            this.vy = 0;
-
-            keyPressed = key.left;
+            if (this.lastDirection != key.right) {
+                this.vx = -this.speed;
+                this.vy = 0;
+                this.lastDirection = key.left;      
+            }
+            
         }
 
-       
-        var head = {x: this.parts[0].x += this.vx, y: this.parts[0].y += this.vy};
+        //Calculate the new position of the head
+        var head = {x: this.parts[0].x + this.vx, y: this.parts[0].y + this.vy};
+        var collided = false;
         if (game.food.collideWith(this)) {
-            this.addPart();
-            console.log("add part");
+            //Do not remove last part if colliding
+            collided = true;
             game.food.reset();
         } else {
+            //Remove the last part of the tail
             this.parts.pop();
         }
+
+        //Add the new head
         this.parts.unshift(head);
+
+        //Check for collision with itself
+        for (var i = 1; i < this.parts.length; i++) {
+            if (head.x == this.parts[i].x && head.y == this.parts[i].y) {
+                game.newGame();
+            }
+        }        
+
+        //Check for out of canvas position
+        if (head.x < 0 || head.y < 0 || head.x > game.width / SCALE || head.y > game.height / SCALE) {
+            game.newGame();
+        }
      
     },
 
     Snake.prototype.render = function(graphics) {
         for(var i = 0; i < this.parts.length; i++) {
-            graphics.drawRect("black", this.parts[i].x * 10, this.parts[i].y * 10, 10, 10)
+            graphics.drawRect("black", this.parts[i].x * SCALE, this.parts[i].y * SCALE, SCALE, SCALE)
         }
-    }
-
-    Snake.prototype.addPart = function() {
-
-
-        document.getElementById("size").innerHTML = ((this.parts.length - 1) / 10) + 1;
-        
     }
 }
 
 function Graphics(ctx) {
     this.ctx = ctx;
-
-    Graphics.prototype.drawLine = function(color, path, width) {
-        console.log(path[0])
-        this.ctx.setFillColor(color);
-        i/*
-        this.ctx.lineWidth= width;
-        this.ctx.beginPath();
-        this.ctx.moveTo(path[0].x, path[0].y );*/
-        for(var i = 0; i < path.length; i++) {
-            var point = path[i];
-            this.drawRect(color, point.x, point.y, 10, 10);    
-        }/*
-        this.ctx.stroke();*/
-    }
-
-    Graphics.prototype.drawRect = function(color, x, y, width, height) {
-        this.ctx.setFillColor(color)
-        this.ctx.fillRect(x - (width / 2), y - (height / 2), width, height);
-    };
-
 }
+
+Graphics.prototype.drawRect = function(color, x, y, width, height) {
+    this.ctx.setFillColor(color)
+    this.ctx.fillRect(x, y, width, height);
+};
 
 function Game() {
     var game = this;
@@ -148,6 +144,9 @@ function Game() {
     this.canvas = document.getElementById("game");
     this.ctx = this.canvas.getContext("2d");
     this.graphics = new Graphics(this.ctx);
+    this.food = [];
+    this.height = this.canvas.height;
+    this.width = this.canvas.width;
 
     window.addEventListener("keydown", function(e) {
         game.keys[e.keyCode] = true;
@@ -157,18 +156,16 @@ function Game() {
         game.keys[e.keyCode] = false;
     }, true);
 
-
-    this.food = new Food(30, 30);
-    this.snake = new Snake(1, 1, this.keys, game);
+    this.newGame();
 
     Game.prototype.start = function() {
+        var last = 0;
         (function loop() {
             requestAnimationFrame(loop);
             game.render();
-            game.update();
         })();
 
-       
+        setInterval(game.update, 1000/20);
     };
 
     Game.prototype.update = function() {
@@ -176,15 +173,22 @@ function Game() {
     };
 
     Game.prototype.render = function() {
-     
         this.ctx.save();
         this.ctx.setTransform(1, 0, 0, 1, 0, 0);
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.restore();
         game.snake.render(this.graphics );
         game.food.render(this.graphics);
-    }
 
+        this.ctx.fillText("Score: " + (this.snake.parts.length - 1), 10, 10)
+    }
+}
+
+Game.prototype.newGame = function() {
+    this.food = new Food(); 
+    this.food.reset();
+    this.snake = new Snake(1, 1, this.keys, this);
+    this.snake.nx = 1;
 }
 
 new Game().start();
